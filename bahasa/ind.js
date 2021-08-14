1100da036bd1010f519aea76e35fb43fc8ca41c8
isall26 <74163516+isall26@users.noreply.github.com>
2021-08-14 15:52:39 +0700

    Add files via upload

---
+++ bahasa/ind.js
@@ -0,0 +1,152 @@
+exports.wait = () => {
+	return`【ᴡᴀɪᴛ】 SEDANG DIPROSES`
+}
+
+exports.succes = () => {
+	return`【𝐒𝐔𝐊𝐒𝐄𝐒】`
+}
+
+exports.pedo = () => {
+	return`【𝐒𝐔𝐊𝐒𝐄𝐒】ᴀᴛɪ ᴀᴛɪ ɢᴀɴ ᴅɪ ɢʀᴇʙᴇᴋ ᴏʟᴇʜ ᴏᴡɴᴇʀ`
+}
+
+exports.lvlon = () => {
+	return`【𝐄𝐍𝐀𝐁𝐋𝐄】 ʟᴇᴠᴇʟɪɴɢ`
+}
+
+exports.lvloff = () => {
+	return`【𝐃𝐈𝐒𝐀𝐁𝐋𝐄】 ʟᴇᴠᴇʟɪɴɢ`
+}
+
+exports.lvlnul = () => {
+	return`𝙇𝙀𝙑𝙀𝙇𝙈𝙐 𝙈𝘼𝙎𝙄𝙃 𝙆𝙊𝙎𝙊𝙉𝙂`
+}
+
+exports.lvlnoon = () => {
+	return`𝙇𝙀𝙑𝙀𝙇 𝘿𝙄 𝙂𝙍𝙐𝘽 𝘽𝙀𝙇𝙐𝙈 𝘿𝙄 𝘼𝙆𝙏𝙄𝙁𝙆𝘼𝙉`
+}
+
+exports.noregis = () => {
+	return`【𝐃𝐀𝐅𝐓𝐀𝐑 𝐃𝐔𝐋𝐔】\n\n 𝐂𝐚𝐫𝐚 𝐝𝐚𝐟𝐭𝐚𝐫 ${prefix}daftar nama|umur \n 𝐜𝐨𝐧𝐭𝐨𝐡${prefix}daftar  ${pushname}|18`
+}
+
+exports.rediregis = () => {
+	return`【𝐒𝐔𝐃𝐀𝐇 𝐓𝐄𝐑𝐃𝐀𝐅𝐓𝐀𝐑】\n\n𝗞𝗮𝗺𝘂 𝘀𝘂𝗱𝗮𝗵 𝘁𝗲𝗿𝗱𝗮𝗳𝘁𝗮𝗿 𝗱𝗶 𝗱𝗮𝘁𝗮𝗯𝗮𝘀𝗲 ${namabot}`
+}
+
+exports.stikga = () => {
+	return`𝗬𝗮𝗵 𝗴𝗮𝗴𝗮𝗹 𝗰𝗼𝗯𝗮 𝘂𝗹𝗮𝗻𝗴𝗶 𝗯𝗲𝗯𝗲𝗿𝗮𝗽𝗮 𝘀𝗮𝗮𝘁 𝗹𝗮𝗴𝗶`
+}
+
+exports.linkga = () => {
+	return`𝗠𝗮𝗮𝗳 𝗹𝗶𝗻𝗸 𝘁𝗶𝗱𝗮𝗸 𝘃𝗮𝗹𝗶𝗱`
+}
+
+exports.groupo = () => {
+	return`【𝐆𝐑𝐎𝐔𝐏 𝐎𝐍𝐋𝐘】`
+}
+
+exports.ownerb = () => {
+	return`【𝗢𝗪𝗡𝗘𝗥 𝗕𝗢𝗧 𝗢𝗡𝗟𝗬】`
+}
+
+exports.ownerg = () => {
+	return`【𝗢𝗪𝗡𝗘𝗥 𝗚𝗥𝗢𝗨𝗣 𝗢𝗡𝗟𝗬】`
+}
+
+exports.admin = () => {
+	return`【𝗔𝗗𝗠𝗜𝗡 𝗚𝗥𝗢𝗨𝗣 𝗢𝗡𝗟𝗬】`
+}
+
+exports.badmin = () => {
+	return`【𝗕𝗢𝗧 𝗛𝗔𝗥𝗨𝗦 𝗝𝗔𝗗𝗜 𝗔𝗗𝗠𝗜𝗡】`
+}
+
+exports.nsfwoff = () => {
+	return`𝐍𝐒𝐅𝐖 𝐓𝐈𝐃𝐀𝐊 𝐀𝐊𝐓𝐈𝐅`
+}
+
+exports.bug = () => {
+	return`𝙼𝚊𝚜𝚊𝚕𝚊𝚑 𝚝𝚎𝚕𝚊𝚑 𝚍𝚒 𝚕𝚊𝚙𝚘𝚛𝚔𝚊𝚗 𝚔𝚎 𝚘𝚠𝚗𝚎𝚛 𝙱𝙾𝚃, 𝙻𝚊𝚙𝚘𝚛𝚊𝚗 𝚙𝚊𝚕𝚜𝚞 𝚊𝚝𝚊𝚞 𝚖𝚊𝚒𝚗² 𝚝𝚒𝚍𝚊𝚔 𝚊𝚔𝚊𝚗 𝚍𝚒𝚝𝚊𝚗𝚐𝚐𝚊𝚙𝚒.`
+}
+
+exports.wrongf = () => {
+	return`𝙁𝙤𝙧𝙢𝙖𝙩 𝙨𝙖𝙡𝙖𝙝/𝙩𝙚𝙭𝙩 𝙠𝙤𝙨𝙤𝙣𝙜`
+}
+
+exports.clears = () => {
+	return`𝘾𝙡𝙚𝙖𝙧 𝙖𝙡𝙡 𝙎𝙪𝙘𝙘𝙚𝙨𝙨`
+}
+
+exports.pc = () => {
+	return`【𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐒𝐈】\n\nUntuk mengetahui apa kamu sudah terdaftar silahkah check message yang saya kirim \n\nNOTE:\n*Jika kamu belum mendapatkan pesan. berarti kamu belum menyimpan nomer bot*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`【𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄】${namabot} 」\n\nkamu sudah terdaftar dengan data \n\n┏━⊱nama\n┗⊱${namaUser}\n┏━⊱nomer\n┗⊱wa.me/${sender.split("@")[0]}\n┏━⊱umur\n┗⊱${umurUser}\n┏━⊱waktu pendaftaran\n┗⊱${time}\n\n┏━❉ *NS* ❉━\n┣⊱${serialUser}\n┗⊱NOTE : 𝐓𝐎𝐋𝐎𝐍𝐆 𝐃𝐎𝐍𝐀𝐒𝐈 𝐁𝐈𝐀𝐑 𝐁𝐎𝐓 𝐀𝐊𝐓𝐈𝐅 𝐓𝐄𝐑𝐔𝐒
+`
+}
+
+exports.owneresce = (pushname) => {
+	return`𝕸𝖆𝖆𝖋 𝖙𝖆𝖕𝖎 ${pushname} 𝖇𝖚𝖐𝖆𝖓 𝖔𝖜𝖓𝖊𝖗 𝕭𝕺𝕿`
+}
+
+exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${aha}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahc}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahd}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahe}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
+	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku, role) => { 
+	return `
+`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel, role) => {
+	return`
+	【𝐒𝐄𝐋𝐀𝐌𝐀𝐓】
+ 📊➤ ɴᴀᴍᴀ : ${pushname}
+ 📊➤ ɴᴏᴍᴇʀ : ${sender.split("@")[0]}
+ 📊➤ xᴘ : ${getLevelingXp(sender)}
+ 📊➤ ʟɪᴍɪᴛ :  +3
+ 📊➤ ᴘᴀɴɢᴋᴀᴛ :  ${role}
+ 📊➤ ʟᴇᴠᴇʟ : ${getLevel} ⊱ ${getLevelingLevel(sender)}
+`}
+ 
+exports.limitend = (pushname) => {
+	return`*Maaf ${pushname} limit hari ini habis*\n*Hubungi kami* : wa.me/6289630267618\n\n*Note : Kami akan berikan random dari 1-10*`
+}
+
+exports.limitcount = (limitCounts) => {
+	return`
+	【𝐋𝐈𝐌𝐈𝐓 𝐂𝐎𝐔𝐍𝐓】
+sisa limit anda : ${limitCounts}
+
+NOTE : 𝙐𝙣𝙩𝙪𝙠 𝙢𝙚𝙣𝙙𝙖𝙥𝙖𝙩𝙠𝙖𝙣 𝙡𝙞𝙢𝙞𝙩. 𝘽𝙞𝙨𝙖 𝙡𝙚𝙬𝙖𝙩 𝙣𝙖𝙞𝙠 𝙡𝙚𝙫𝙚𝙡 𝙢𝙚𝙡𝙖𝙡𝙪𝙞 𝙜𝙧𝙤𝙪𝙥 𝙖𝙩𝙖𝙪 𝙗𝙪𝙮𝙡𝙞𝙢𝙞𝙩.
+`}
+
+exports.satukos = () => {
+	return`【𝙏𝙖𝙢𝙗𝙖𝙝 𝙥𝙖𝙧𝙖𝙢𝙚𝙩𝙚𝙧 1/𝙚𝙣𝙖𝙗𝙡𝙚 𝙖𝙩𝙖𝙪 0/𝙙𝙞𝙨𝙖𝙗𝙡𝙚】`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`◪ *【𝐀𝐓𝐌 𝐁𝐁𝐈 (𝐁𝐀𝐍𝐊 𝐁𝐎𝐓 𝐈𝐒𝐀𝐋𝐋)】*\n  ├─ ❏ *Name* : ${pushname}\n  ├─ ❏ *Nomor* : ${sender.split("@")[0]}\n  └─ ❏ *Uang* : ${uangkau}
+`}
